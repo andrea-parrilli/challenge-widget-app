ec3932ccc2e08ca46214c2e31ffdfab76cc027a3
AP <andrea.parrilli@gmail.com>
2024-05-07 18:05:03 +0200

    Use record for error response dto and reformat code

--- challenge-widget-server/src/main/java/ma/ap/challenge/widgetapp/server/ErrorMappings.java
+++ challenge-widget-server/src/main/java/ma/ap/challenge/widgetapp/server/ErrorMappings.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
-import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ConstraintViolationException;
 import java.time.ZonedDateTime;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -30,37 +28,37 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 public class ErrorMappings {
     private static final Logger LOGGER = LoggerFactory.getLogger(ErrorMappings.class);
 
-    private ResponseEntity<ErrorResponse> obfuscate(Exception e, HttpServletRequest request) {
+    private ResponseEntity<ErrorResponse> obfuscate(Exception e,
+                                                    HttpServletRequest request) {
         LOGGER.warn("Exception during request", e);
-        return error(
-                "An internal server error occurred. Please contact support if this issue persists.",
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                request
-        );
+        return error("An internal server error occurred. Please contact support if this issue persists.",
+                     HttpStatus.INTERNAL_SERVER_ERROR,
+                     request);
     }
 
-    private ResponseEntity<ErrorResponse> error(Exception e, HttpStatus status, HttpServletRequest request) {
+    private ResponseEntity<ErrorResponse> error(Exception e,
+                                                HttpStatus status,
+                                                HttpServletRequest request) {
         LOGGER.info("Exception during request", e);
         return error(e.getMessage(), status, request);
     }
 
-    private ResponseEntity<ErrorResponse> error(String message, HttpStatus status, HttpServletRequest request) {
+    private ResponseEntity<ErrorResponse> error(String message,
+                                                HttpStatus status,
+                                                HttpServletRequest request) {
         return ResponseEntity.status(status)
-                .body(new ErrorResponse(
-                        ZonedDateTime.now(),
-                        status.value(),
-                        status.getReasonPhrase(),
-                        message,
-                        request.getRequestURI()));
+                .body(new ErrorResponse(ZonedDateTime.now(),
+                                        status.value(),
+                                        status.getReasonPhrase(),
+                                        message,
+                                        request.getRequestURI()));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(MethodArgumentTypeMismatchException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentTypeMismatchException e,
+                                                HttpServletRequest request) {
         if (e.getCause() instanceof IllegalArgumentException) {
-            return error((IllegalArgumentException) e.getCause(),
-                    BAD_REQUEST,
-                    request
-            );
+            return error((IllegalArgumentException) e.getCause(), BAD_REQUEST, request);
         }
 
         return obfuscate(e, request);
@@ -73,7 +71,8 @@ public class ErrorMappings {
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(HttpMessageNotReadableException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handle(HttpMessageNotReadableException e,
+                                                HttpServletRequest request) {
         var cause = e.getCause();
         if (cause instanceof ValueInstantiationException causeException) {
             return handle(causeException, request);
@@ -91,65 +90,63 @@ public class ErrorMappings {
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(MismatchedInputException e, HttpServletRequest request) {
-        var message = String.format("The structure or type of '%s' is invalid", e.getPath().stream()
-                .map(JsonMappingException.Reference::getFieldName)
-                .collect(Collectors.joining(".")));
+    public ResponseEntity<ErrorResponse> handle(MismatchedInputException e,
+                                                HttpServletRequest request) {
+        var message = String.format("The structure or type of '%s' is invalid",
+                                    e.getPath()
+                                            .stream()
+                                            .map(JsonMappingException.Reference::getFieldName)
+                                            .collect(Collectors.joining(".")));
         return error(message, BAD_REQUEST, request);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(ValueInstantiationException e, HttpServletRequest request) {
-        var message = String.format("Field '%s' is invalid", e.getPath().stream()
-                .map(JsonMappingException.Reference::getFieldName)
-                .collect(Collectors.joining(".")));
+    public ResponseEntity<ErrorResponse> handle(ValueInstantiationException e,
+                                                HttpServletRequest request) {
+        var message = String.format("Field '%s' is invalid",
+                                    e.getPath()
+                                            .stream()
+                                            .map(JsonMappingException.Reference::getFieldName)
+                                            .collect(Collectors.joining(".")));
         return error(message, BAD_REQUEST, request);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(IllegalArgumentException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handle(IllegalArgumentException e,
+                                                HttpServletRequest request) {
         return error(e, BAD_REQUEST, request);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(NoSuchElementException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handle(NoSuchElementException e,
+                                                HttpServletRequest request) {
         return error(e, HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(ConstraintViolationException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handle(ConstraintViolationException e,
+                                                HttpServletRequest request) {
         return error(e, BAD_REQUEST, request);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(IllegalStateException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handle(IllegalStateException e,
+                                                HttpServletRequest request) {
         return obfuscate(e, request);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(MissingServletRequestPartException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handle(MissingServletRequestPartException e,
+                                                HttpServletRequest request) {
         return error(e, BAD_REQUEST, request);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handle(Exception e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handle(Exception e,
+                                                HttpServletRequest request) {
         return obfuscate(e, request);
     }
 
-    @Getter
-    public static class ErrorResponse {
-        private final ZonedDateTime timestamp;
-        private final Integer status;
-        private final String error;
-        private final String message;
-        private final String path;
-
-        public ErrorResponse(ZonedDateTime timestamp, Integer status, String error, String message, String path) {
-            this.timestamp = timestamp;
-            this.status = status;
-            this.error = error;
-            this.message = StringUtils.isEmpty(message) ? error : message;
-            this.path = path;
-        }
+    public record ErrorResponse(ZonedDateTime timestamp, Integer status, String error, String message, String path) {
     }
 }
